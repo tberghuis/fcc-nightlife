48f61fff82e528659d751effb322350c3d399b06
Your Name <you@example.com>
2017-06-13 17:44:42 +1000

    .

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,10 @@
 import React from 'react';
+
+
+import { Field, reduxForm } from 'redux-form';
+
+
+
 // import agent from '../agent';
 // import { connect } from 'react-redux';
 // import { Container, Input, Button, Form } from 'semantic-ui-react';
@@ -7,12 +13,40 @@ import React from 'react';
 
 class Login extends React.Component {
 
-    render() {
 
+    // bad naming
+    xhrPostLogin = () => {
+        console.log('test');
+
+    }
+
+    render() {
+        const { handleSubmit, pristine, reset, submitting } = this.props;
         return (
-            <div>login</div>
+            <form onSubmit={handleSubmit(this.xhrPostLogin)}>
+                <div>
+                    <label>Email</label>
+                    <div>
+                        <Field
+                            name="email"
+                            component="input"
+                            type="email"
+                            placeholder="Email"
+                        />
+                    </div>
+                </div>
+                <div>
+                    {/*<button type="submit" disabled={pristine || submitting}>Submit</button>*/}
+                    <button type="submit">Submit</button>
+
+                </div>
+            </form>
         );
     }
 }
 
-export default Login;
+// export default Login;
+
+export default reduxForm({
+    form: 'login'
+})(Login)
